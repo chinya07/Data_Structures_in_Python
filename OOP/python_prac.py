4ef364e31e475cbcfe0aa7d818a082199456ee8e
Chinmaya Kaundanya <kaundanyachinmaya07@gmail.com>
2018-12-26 10:02:33 +0530

    added games folder

---
+++ OOP/python_prac.py
@@ -0,0 +1,16 @@
+#Author: CHINMAYA KAUNDANYA
+#In this program we will see how to define a class
+
+class MyClass:
+
+    #class instances
+
+    var = 10
+
+classObj1 = MyClass()
+print(classObj1)        #printing class object's memory in hex
+print(classObj1.var)
+
+classObj2 = MyClass()
+print(classObj2)        #printing class object's memory in hex
+print(classObj2.var)
